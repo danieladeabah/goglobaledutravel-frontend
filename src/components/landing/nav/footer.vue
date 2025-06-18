42f5e6fda0cf05ea9a3b12f0da4331b0ccf2ff05
danieladeabah <daniel.adeabah@amalitech.com>
2025-06-18 19:59:43 +0000

    Refactor footer component to enhance grid layout and improve styling for links

--- src/components/landing/nav/footer.vue
+++ src/components/landing/nav/footer.vue
@@ -6,15 +6,20 @@
       <div class="copyright">
         Copyright © 2025 Canam Group All rights reserved.
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-3">
+      <div
+        class="grid grid-cols-3 gap-0 border-t border-gray-200 pt-4 md:pt-0 lg:gap-3 lg:border-0"
+      >
         <nuxt-link to="/disclaimer" class="text-gray-500 hover:underline"
           >Disclaimer</nuxt-link
         >
         <nuxt-link to="/privacy-policy" class="text-gray-500 hover:underline"
           >Privacy Policy</nuxt-link
         >
-        <nuxt-link to="/terms-conditions" class="text-gray-500 hover:underline"
-          >Terms & Conditions</nuxt-link
+        <nuxt-link
+          to="/terms-conditions"
+          title="Terms & Conditions"
+          class="text-gray-500 hover:underline"
+          >Terms & C..</nuxt-link
         >
       </div>
     </div>
