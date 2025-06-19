5b20714ae58b184b56cdbdea830fb958ce0e297a
danieladeabah <daniel.adeabah@amalitech.com>
2025-06-19 01:45:41 +0000

    Update page title for improved clarity and branding

--- meta-config.ts
+++ meta-config.ts
@@ -1,6 +1,27 @@
 export const meta = {
-  title: 'Study Abroad',
+  title: 'Study Abroad and Overseas Education | GoGlobalEduTravel',
   htmlAttrs: {
     lang: 'en'
-  }
+  },
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png'
+    },
+    { rel: 'manifest', href: '/site.webmanifest' }
+  ]
 }
